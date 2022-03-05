78d5a8b14a9cb17aa2dd95b68a5a4b09b489a15f
Vincenzo Domina <vincenzo.domina@googlemail.com>
2022-03-05 21:33:14 +0100

    feat: fix links, toggle theme

--- src/layouts/MainLayout/Settings.tsx
+++ src/layouts/MainLayout/Settings.tsx
@@ -12,14 +12,25 @@ import {
   Sun as LightIcon,
   Moon as DarkIcon
 } from 'react-feather';
+import useSettings from 'src/hooks/useSettings';
+import { THEMES } from 'src/constants';
 
 const Settings: FC = () => {
   const ref = useRef<any>(null);
+  const { settings, saveSettings } = useSettings();
   const [isDark, setIsDark] = useState<boolean>(false);
 
   const handleChangeTheme = async () => {
+    const newThemeSetting = isDark ? THEMES.LIGHT : THEMES.DARK; // Toggle Theme
     // Set theme before db
     setIsDark(previousValue => !previousValue);
+
+    // Settings Context // TODO: del and use redux 
+    saveSettings({
+      direction: 'ltr',
+      responsiveFontSizes: true,
+      theme: newThemeSetting
+    });
   };
 
   return (
